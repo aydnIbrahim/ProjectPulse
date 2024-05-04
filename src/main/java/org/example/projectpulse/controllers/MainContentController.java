605be2e17aa1baa906cf8e581ededd4ffeddad53
İbrahim Aydın <aydnibrahim22@gmail.com>
2024-05-04 15:09:32 +0300

    Added basic main-content.fxml and project-content contents.

---
+++ src/main/java/org/example/projectpulse/controllers/MainContentController.java
@@ -0,0 +1,23 @@
+package org.example.projectpulse.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainContentController {
+
+    @FXML
+    private Button dependenciesButton;
+
+    @FXML
+    private Button designsButton;
+
+    @FXML
+    private Button projectTab;
+
+    @FXML
+    private Button reportsButton;
+
+    @FXML
+    private Button tasksButton;
+
+}
--- src/main/java/org/example/projectpulse/controllers/MainPageController.java
+++ src/main/java/org/example/projectpulse/controllers/MainPageController.java
@@ -1,14 +1,27 @@
 package org.example.projectpulse.controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 
 public class MainPageController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Button carcounterTab;
+
+    @FXML
+    private Button drugtrackingsystemTab;
+
+    @FXML
+    private AnchorPane mainPageAnchor;
+
+    @FXML
+    private Button msocialTab;
+
+    @FXML
+    private Button recipegenTab;
+
+    @FXML
+    private Button todolistTab;
+
 }
